ea5a9af4b4602284e76839a2f25cbdfe486b397f
julia.nemtseva <juliaiskra@gmail.com>
2021-08-22 21:47:33 +0300

    Complete Quickcheck assignment

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -6,7 +6,14 @@ import Gen.*
 import Prop.forAll
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for {
+      e <- arbitrary[A]
+      h <- oneOf(const(empty), genHeap)
+    } yield insert(e, h)
+  )
+
   given Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
@@ -14,3 +21,60 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
     findMin(insert(m, h)) == m
   }
 
+  property("min1") = forAll { (a: Int) =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val min = if a < b then a else b
+    val h = insert(a, insert(b, empty))
+    findMin(h) == min
+  }
+
+  property("delete1") = forAll { (a: Int) =>
+    val h = insert(a, empty)
+    isEmpty(deleteMin(h)) == true
+  }
+
+  property("sorting") = forAll { (a: Int, b: Int, c: Int) =>
+    val list = List(a, b, c)
+    val h = insertAll(list, empty)
+    val elements = deleteRecur(h, List())
+    elements == list.sorted
+  }
+
+  property("melding") = forAll { (h1: H, h2: H) =>
+    if isEmpty(h1) && isEmpty(h2)
+    then true
+    else if isEmpty(h1)
+    then findMin(meld(h1, h2)) == findMin(h2)
+    else if isEmpty(h2)
+    then findMin(meld(h1, h2)) == findMin(h1)
+    else {
+      val min1 = findMin(h1)
+      val min2 = findMin(h2)
+      val min = if min1 < min2 then min1 else min2
+      findMin(meld(h1, h2)) == min
+    }
+  }
+
+  property("contains all inserted elements") = forAll { (a: Int, b: Int) =>
+    val list = List(a, b)
+    val h = insertAll(list, empty)
+    val elements = deleteRecur(h, List())
+    list.toSet == elements.toSet
+  }
+
+  def insertAll(elements: List[Int], h: H): H =
+    elements match {
+      case Nil => h
+      case e :: es => insertAll(es, insert(e, h))
+    }
+
+  def deleteRecur(h: H, acc: List[A]): List[A] =
+    if isEmpty(h) then acc
+    else {
+      val min = findMin(h)
+      deleteRecur(deleteMin(h), acc :+ min)
+    }
