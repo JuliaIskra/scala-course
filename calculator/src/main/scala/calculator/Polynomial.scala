c5077383e653045a2ef0f2205bbdc07504ec0843
julia.nemtseva <juliaiskra@gmail.com>
2022-06-26 17:08:16 +0300

    Implement 2nd order polynomial solver

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,8 +3,16 @@ package calculator
 object Polynomial extends PolynomialInterface:
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] =
-    ???
+    Signal.Var(b() * b() - 4 * a() * c())
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
-    ???
+    Signal.Var {
+      if (delta() < 0) Set()
+      else {
+        val r1 = (-b() - Math.sqrt(delta())) / (2 * a())
+        val r2 = (-b() + Math.sqrt(delta())) / (2 * a())
+        if (r1 == r2) Set(r1)
+        else Set(r1, r2)
+      }
+    }
